1297de6d4ce20af7bed2ecc164359610fa49b989
OX-TOPIS <anthicha234@gmail.com>
2023-12-27 02:39:09 +0700

    add

---
+++ frontend/src/pages/home/Testimonials.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+const Testimonials = () => {
+  return (
+    <div className="section-container">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-12">
+        <div className="md:w-1/2">
+          <img src="/images/recipes/img2.png" alt="" className="w-10/12 mx-auto" />
+        </div>
+        <div className="md:w-1/2">
+          <div className="text-left md:w-4/5">
+            <p className="subtitle">Testimonials</p>
+            <h2 className="title">What Our Customers Say About Us</h2>
+            <blockquote className="my-5 text-secondary leading-[30px]">
+              “I had the pleasure of dining at Foodi last night, and I'm still
+              raving about the experience! The attention to detail in
+              presentation and service was impeccable”
+            </blockquote>
+           
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
